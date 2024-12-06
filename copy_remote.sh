7d10aa8e5549c5ff60ec336dddf3a50728e9f307
Peter Lord <peter@plord.co.uk>
2024-12-06 10:47:30 +0000

    Fix remote copy

--- copy_remote.sh
+++ copy_remote.sh
@@ -35,40 +35,39 @@ RESTIC="sudo -E -u www-data restic --no-cache"
 
 # add tag for phone
 #
-${RESTIC} tag --set Wokingham $(${RESTIC} snapshots --json --host "Peter's Galaxy S20 FE 5G" | jq -r '.[] | "\(.short_id)"')
+${RESTIC} tag --set Wokingham --host "Peter's Galaxy S20 FE 5G"
 
 # Wok -> Ply
 #
 echo "Wokingham -> Plymouth"
 export RESTIC_FROM_REPOSITORY=${WOK_REPO}
 export RESTIC_REPOSITORY=${PLY_REPO}
-out=$(${RESTIC} --repo ${PLY_REPO} copy --from-repo ${WOK_REPO} $(${RESTIC} --repo ${WOK_REPO} snapshots --latest 1 --tag Wokingham --json | jq -r '.[] | "\(.short_id)"') 2>&1) || mosquitto_pub -t homeassistant/infoalert -m "$(uname -n) $0: $(echo ${out} | tail -20)"
-echo ${out}
+${RESTIC} --repo ${PLY_REPO} copy --from-repo ${WOK_REPO} --tag Wokingham 2>/tmp/resticcopy.log || mosquitto_pub -t homeassistant/infoalert -m "$(uname -n) $0: $(cat /tmp/resticcopy.log)"
+cat /tmp/resticcopy.log
 
 # Ply -> Wok
 #
 echo "Plymouth -> Wokingham"
 export RESTIC_FROM_REPOSITORY=${PLY_REPO}
 export RESTIC_REPOSITORY=${WOK_REPO}
-out=$(${RESTIC} --repo ${WOK_REPO} copy --from-repo ${PLY_REPO} $(${RESTIC} --repo ${PLY_REPO} snapshots --latest 1 --tag Plymouth --json | jq -r '.[] | "\(.short_id)"') 2>&1) || mosquitto_pub -t homeassistant/infoalert -m "$(uname -n) $0: $(echo ${out} | tail -20)"
-echo ${out}
+${RESTIC} --repo ${WOK_REPO} copy --from-repo ${PLY_REPO} --tag Plymouth 2>/tmp/resticcopy.log || mosquitto_pub -t homeassistant/infoalert -m "$(uname -n) $0: $(cat /tmp/resticcopy.log)"
+cat /tmp/resticcopy.log
 
 echo "Plymouth latest"
 ${RESTIC} --repo ${PLY_REPO} snapshots --latest 1
+echo "Wokingham latest"
+${RESTIC} --repo ${WOK_REPO} snapshots --latest 1
 
 # update plymouth status
 #
 ${RESTIC} --repo ${PLY_REPO} snapshots --latest 1 --json  | jq -r '.[] | "\(.hostname)|\(.paths[0])"' | while IFS="|" read -r hostname paths 
 do 
-  if [ "${paths}" = "/var/AmazonCopy" -o "${paths}" = "/var/google/plord1250-drive" ]
+  if [ "${paths}" != "/var/AmazonCopy" -a "${paths}" != "/var/google/plord1250-drive" ]
   then
-    # special case - skip old ones
-    mosquitto_pub -r -t "homeassistant/sensor/restic/${id}/config" -m ""
-    mosquitto_pub -r -t "homeassistant/sensor/restic/${id}/attributes" -m ""
-  else 
-    echo "updating status ${hostname} ${paths}"
+    echo "updating plymouth status ${hostname} ${paths}"
 
-    RESTICHOSTNAME="${hostname}" RESTIC_REPOSITORY=${PLY_REPO} IDPREFIX=remoterestic success "${paths}"
+    RESTICHOSTNAME="${hostname}" RESTIC_REPOSITORY=${PLY_REPO} IDPREFIX=remote_restic create_topic "${paths}"
+    RESTICHOSTNAME="${hostname}" RESTIC_REPOSITORY=${PLY_REPO} IDPREFIX=remote_restic success "${paths}"
   fi
 done
 
@@ -76,8 +75,9 @@ done
 #
 ${RESTIC} snapshots --latest 1 --json --host arm5 --host "Peter's Galaxy S20 FE 5G" | jq -r '.[] | "\(.hostname)|\(.paths[0])"' | while IFS="|" read -r hostname paths 
 do 
-  echo "updating status ${hostname} ${paths}"
+  echo "updating missing status ${hostname} ${paths}"
 
+  RESTICHOSTNAME="${hostname}" RESTIC_REPOSITORY=${WOK_REPO} create_topic "${paths}"
   RESTICHOSTNAME="${hostname}" RESTIC_REPOSITORY=${WOK_REPO} success "${paths}"
 done
 

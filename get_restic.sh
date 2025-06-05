4785d23f09deafcadf65f5361bf3410726dabceb
Peter Lord <peter@plord.co.uk>
2025-06-05 08:01:06 +0100

    Update rest server version

--- get_restic.sh
+++ get_restic.sh
@@ -17,6 +17,7 @@ then
 fi
 
 restic_ver=0.18.0
+restserver_ver=0.14.0
 
 set -e
 
@@ -31,9 +32,9 @@ else
 fi
 rm restic_${restic_ver}_${os}_${arch} 
 
-curl -L -O https://github.com/restic/rest-server/releases/download/v0.13.0/rest-server_0.13.0_${os}_${arch}.tar.gz
-tar -xvzf rest-server_0.13.0_${os}_${arch}.tar.gz
-chmod a+x rest-server_0.13.0_${os}_${arch}/rest-server
-${SUDO} cp rest-server_0.13.0_${os}_${arch}/rest-server /usr/local/bin/rest-server
-rm rest-server_0.13.0_${os}_${arch}.tar.gz
-rm -rf rest-server_0.13.0_${os}_${arch}
+curl -L -O https://github.com/restic/rest-server/releases/download/v${restserver_ver}/rest-server_${restserver_ver}_${os}_${arch}.tar.gz
+tar -xvzf rest-server_${restserver_ver}_${os}_${arch}.tar.gz
+chmod a+x rest-server_${restserver_ver}_${os}_${arch}/rest-server
+${SUDO} cp rest-server_${restserver_ver}_${os}_${arch}/rest-server /usr/local/bin/rest-server
+rm rest-server_${restserver_ver}_${os}_${arch}.tar.gz
+rm -rf rest-server_${restserver_ver}_${os}_${arch}
